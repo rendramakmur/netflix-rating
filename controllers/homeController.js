970838c64cd31e8ef424b9f78c9d15585a57fecd
rendramakmur <rendra1795@gmail.com>
2021-02-25 15:38:30 +0700

    Add login page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,6 +6,21 @@ class HomeController {
     static register(req, res) {
         res.render('register')
     }
+
+    static getLogin(req, res) {
+        res.render('login');
+    }
+
+    static postLogin(req, res) {
+        let user = {
+            username: req.body.username,
+            password: req.body.password
+        }
+
+        console.log(user);
+
+        // Belum dihashing dan belum validate username & passwordnya, setelah login redirect ke home '/'
+    }
 }
 
 module.exports = HomeController;
